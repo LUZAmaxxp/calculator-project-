4e2c0dfec7aee83a068d1799555e00a9234efc20
LUZAmaxxp <ayman.allouch@e-polytechnique.ma>
2024-01-22 22:14:35 +0100

    this the final commit of my project

--- calculator.js
+++ calculator.js
@@ -1,44 +1,101 @@
-const calculator = document.querySelector(".calculator");
-const keys = calculator.querySelector(".calculator-keys");
-
-keys.addEventListener("click", (s) => {
-  if (s.target.matches("button")) {
-    console.log(s.target.innerText);
+const calculate = (n1, operator, n2) => {
+  let result = "";
+  if (operator === "add") {
+    result = parseFloat(n1) + parseFloat(n2);
+  } else if (operator === "subtract") {
+    result = parseFloat(n1) - parseFloat(n2);
+  } else if (operator === "multiply") {
+    result = parseFloat(n1) * parseFloat(n2);
+  } else if (operator === "divide") {
+    result = parseFloat(n1) / parseFloat(n2);
   }
-  const key = s.target;
-  const action = key.dataset.action;
 
+  return result;
+};
+function isNumeric(str) {
+  if (typeof str != "string") return false; //
+  return !isNaN(str) && !isNaN(parseFloat(str));
+}
+
+const clear = (display) => {
+  display.textContent = "0";
+};
+
+const displayednum = (
+  action,
+  displayedNum,
+  previousKeyType,
+  keyContent,
+  calculator,
+  display
+) => {
   if (!action) {
-    console.log("number key!");
-  } else if (
-    action == "add" ||
-    action == "substract" ||
-    action == "multiply" ||
-    action == "divide"
-  ) {
-    console.log("operator key!");
+    if (displayedNum === "0" || previousKeyType === "operator") {
+      if (isNumeric(keyContent)) {
+        calculator.dataset.previousKeyType = undefined;
+      }
+      display.textContent = keyContent;
+    } else {
+      display.textContent = displayedNum + keyContent;
+    }
   }
-  if (action == "decimal") console.log("decimal key!");
-  if (action == "clear") console.log("clear key!");
-  if (action == "calculate") console.log("calculate!");
-});
-const display = document.querySelector(".calculator-display");
+};
 
-keys.addEventListener("click", (s) => {
-  if (s.target.matches("button")) {
-    const key = s.target;
+const calculator = document.querySelector(".calculator");
+const display = calculator.querySelector(".calculator-display");
+const keys = document.querySelector(".calculator-keys");
+
+keys.addEventListener("click", (e) => {
+  if (e.target.matches("button")) {
+    const key = e.target;
     const action = key.dataset.action;
-    const keycontent = key.textContent;
-    const displayNum = display.textContent;
-    if (!action) {
-      if (displayNum == "0") {
-        display.textContent = keycontent;
-      } else {
-        display.textContent = displayNum + keycontent;
-      }
-      if (action == "decimal") {
-        display.textContent = displayNum + ".";
-      }
+    const keyContent = key.textContent;
+
+    const displayedNum = display.textContent;
+    let previousKeyType = calculator.dataset.previousKeyType;
+    console.log(previousKeyType);
+    console.log(action);
+    Array.from(key.parentNode.children).forEach((k) =>
+      k.classList.remove("is-depressed")
+    );
+
+    displayednum(
+      action,
+      displayedNum,
+      previousKeyType,
+      keyContent,
+      calculator,
+      display
+    );
+
+    if (action === "decimal") {
+      display.textContent = displayedNum + ".";
+    }
+
+    if (
+      action === "add" ||
+      action === "subtract" ||
+      action === "multiply" ||
+      action === "divide"
+    ) {
+      key.classList.add("is-depressed");
+      console.log("setting op");
+      calculator.dataset.previousKeyType = "operator";
+      calculator.dataset.firstValue = displayedNum;
+      calculator.dataset.operator = action;
+    }
+
+    if (action === "clear") {
+      console.log("clear key!");
+      clear(display);
+    }
+
+    if (action === "calculate") {
+      const firstValue = calculator.dataset.firstValue;
+      const operator = calculator.dataset.operator;
+      const secondValue = displayedNum;
+
+      display.textContent = calculate(firstValue, operator, secondValue);
     }
   }
 });
