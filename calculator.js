4a335703d5a4a4321ce98b33629aa9146a0ac562
LUZAmaxxp <ayman.allouch@e-polytechnique.ma>
2024-01-22 01:08:57 +0100

    added a function that consoles in the BOM which key you clicked and display the numbers clicked

--- calculator.js
+++ calculator.js
@@ -5,4 +5,40 @@ keys.addEventListener("click", (s) => {
   if (s.target.matches("button")) {
     console.log(s.target.innerText);
   }
+  const key = s.target;
+  const action = key.dataset.action;
+
+  if (!action) {
+    console.log("number key!");
+  } else if (
+    action == "add" ||
+    action == "substract" ||
+    action == "multiply" ||
+    action == "divide"
+  ) {
+    console.log("operator key!");
+  }
+  if (action == "decimal") console.log("decimal key!");
+  if (action == "clear") console.log("clear key!");
+  if (action == "calculate") console.log("calculate!");
+});
+const display = document.querySelector(".calculator-display");
+
+keys.addEventListener("click", (s) => {
+  if (s.target.matches("button")) {
+    const key = s.target;
+    const action = key.dataset.action;
+    const keycontent = key.textContent;
+    const displayNum = display.textContent;
+    if (!action) {
+      if (displayNum == "0") {
+        display.textContent = keycontent;
+      } else {
+        display.textContent = displayNum + keycontent;
+      }
+      if (action == "decimal") {
+        display.textContent = displayNum + ".";
+      }
+    }
+  }
 });
